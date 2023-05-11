06a47b9b74737a390179b97b01a1efdcb620744d
BowTiedBettor <109925930+BowTiedBettor@users.noreply.github.com>
2023-05-11 19:21:48 +0200

    Update nhlcollectdata.py

--- BayesNHL/nhlapi.py
+++ BayesNHL/nhlapi.py
@@ -1,22 +1,31 @@
 import requests
 import time
 import numpy as np
+import pandas as pd
+import xlsxwriter
 from traceback import print_exc
 
 season = "2022"
 game_type = "02" # 02 is for regular season games
 
+games = []
 cases = []
+scores = []
 
 game_nr = 1
 while True: 
     try: 
-        # generate the url & fetch game data [you could store the info youre interested in & build your own db ofc, we don't care about that for now]
+        # generate the url & fetch game data [you could store any info youre interested in & build your own db ofc]
         game_nr_str = str(game_nr)
         while len(game_nr_str) < 4: 
             game_nr_str = "0" + game_nr_str
         game_data = requests.get(f"https://statsapi.web.nhl.com/api/v1/game/{season}{game_type}{game_nr_str}/feed/live").json()
         linescore = game_data['liveData']['linescore']
+
+        # collect basic info for the game and append to "games" list
+        home_team = game_data['gameData']['teams']['home']['name']
+        away_team = game_data['gameData']['teams']['away']['name']
+        games.append(f"{home_team} v {away_team}")
 
         # run the analysis
         home_goals = 0
@@ -33,14 +42,21 @@ while True:
                 cases.append(1)
             else: 
                 cases.append(0)
-        if second_period_away_goals > second_period_home_goals: 
+        elif second_period_away_goals > second_period_home_goals: 
             if away_goals > home_goals: 
                 cases.append(1)
             else: 
                 cases.append(0)
+        else: 
+            cases.append(None)
+
+        # store final score
+        scores.append(f"{home_goals} - {away_goals}")
 
         # uncomment if an api disrespectooor
-        time.sleep(.1)
+        # time.sleep(.1)
+
+        print(f"{game_nr}/1312 games analyzed")
 
         game_nr += 1
 
@@ -49,6 +65,19 @@ while True:
         print_exc()
         break
 
-fraction = np.average(cases)
-print(fraction)
-print(len(cases))
+# create a pd.DataFrame & dump into an excel file in the current directory
+columns = {
+    'Game': games,
+    'Score': scores,
+    'Case': cases,
+}
+
+nhl_data_df = pd.DataFrame(columns)
+
+workbook = xlsxwriter.Workbook("NHLData.xlsx")
+workbook.close()
+with pd.ExcelWriter("NHLData.xlsx", mode='a', if_sheet_exists='overlay', engine = 'openpyxl') as writer:
+    nhl_data_df.to_excel(writer)
+
+
+
